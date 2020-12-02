ff678e1cc8164f1b64b0030b5ab2cbaf479df042
Xenvex <matthewbreyes@gmail.com>
2020-12-01 18:49:17 -0800

    PickUpSticks

--- MandoGame/Assets/Matt/Scripts/PickUpBox.cs
+++ MandoGame/Assets/Matt/Scripts/PickUpBox.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PickUpBox : MonoBehaviour
 {
+    private float healthCompare = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,11 +19,32 @@ public class PickUpBox : MonoBehaviour
 
     void OnTriggerEnter()
     {
-        //Gives the item to the player
-        /*
-        GameObject thePlayer = GameObject.Find("Player");
-        PlayerScript playerScript = thePlayer.GetComponent<Player>();
-        playerScript.Item = true;
-        */
+        //Accessing the player script for reasons
+        GameObject thePlayer = GameObject.Find("PlayerTest");
+        PlayerTest playerScript = thePlayer.GetComponent<PlayerTest>();
+        //Gives the item according to what item this script is attached to.
+        if(gameObject.name == "Blaster")
+        {
+            //Enables Player to shoot
+            playerScript.Blaster = true;
+        }
+        else if (gameObject.name == "Grenade")
+        {
+            //Gives Player Grenade
+            playerScript.Grenade += 1;
+        }
+        else if (gameObject.name == "HealthPack")
+        {
+            //Increases health, For loop to make sure it doesn't overheal
+            healthCompare = playerScript.health;
+            for(int i = 0; i < 10; i++)
+            {
+                if(healthCompare < 100)
+                {
+                    playerScript.health += 1;
+                }
+            }
+        }
+        
     }
 }
